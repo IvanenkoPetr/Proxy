d6bbb65c329390e7ce7c09f6a74a5e12e11eb9a3
IvanenkoPetr <peter98765@rambler.ru>
2021-04-30 07:34:07 +0300

    Initial commit

---
+++ Proxy/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            IPAddress localHostAddres = IPAddress.Parse("127.0.0.1");
+            int port = 1234;
+            TcpListener server = new TcpListener(localHostAddres, port);       
+            server.Start();
+  
+            while (true)
+            {
+                try
+                {
+                    // Подключение клиента
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream clientStream = client.GetStream();
+
+                    // Обмен данными
+                    try
+                    {
+
+                        var completeClientMessage = StreamUtilities.ReadStream(clientStream);
+
+                        Console.WriteLine(completeClientMessage.Count());
+
+                        Task.Run(delegate
+                        {
+                            var responseFromServer = StreamUtilities.GetServerResponse(1240, completeClientMessage);
+                            clientStream.Write(responseFromServer, 0, responseFromServer.Length);
+
+                            clientStream.Close();
+                            client.Close();
+                        }
+                        );          
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    finally
+                    {
+                  
+                    }
+                }
+                catch
+                {
+                    server.Stop();
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Proxy/StreamUtilities.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Proxy
+{
+    public static class StreamUtilities
+    {
+
+        public static byte[] ReadStream(NetworkStream stream)
+        {
+            
+            IEnumerable<byte> result = new List<byte>();
+            if (stream.CanRead)
+            {
+                byte[] readBuffer = new byte[1024];
+                do
+                {                    
+                    var numberOfBytesRead = stream.Read(readBuffer, 0, readBuffer.Length);                   
+                    result = result.Concat(readBuffer.Take(numberOfBytesRead));
+                }
+                while (stream.DataAvailable);
+            }
+            return result.ToArray();
+
+        }
+
+        public static byte[] GetServerResponse(int server, byte[] clientRequest)
+        {
+            TcpClient TcpClientForServer = new TcpClient();
+            TcpClientForServer.Connect("127.0.0.1", server);
+
+            var serverStream = TcpClientForServer.GetStream();
+            serverStream.Write(clientRequest, 0, clientRequest.Length);
+
+            var responseFromServer = ReadStream(serverStream);
+
+            serverStream.Close();
+            TcpClientForServer.Close();
+
+            return responseFromServer;
+            //return new byte[1];
+
+
+        }
+
+    }
+}
